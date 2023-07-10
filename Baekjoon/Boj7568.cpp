b330848a8d4fb8dbffa73baff0dfe0a4b62ee557
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-07-10 12:02:32 +0900

    Boj7568

--- Baekjoon/Boj7568.cpp
+++ Baekjoon/Boj7568.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main() {
-	int n;
-	int x, y;
-	cin >> n;
+    int num;
+    int rank = 1;
+
+    pair<int, int> arr[50];
+
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+        cin >> arr[i].first >> arr[i].second;
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num; j++)
+            if (arr[i].first < arr[j].first && arr[i].second < arr[j].second)
+                rank++;
+        cout << rank << ' ';
+        rank = 1;
+    }
 }
