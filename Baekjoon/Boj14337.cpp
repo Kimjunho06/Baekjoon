63b218ac041ec73c91be1ba2ab4d22033e0898ee
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-07-13 14:21:11 +0900

    Visual Basic Boj

---
+++ Baekjoon/Boj14337.cpp
@@ -0,0 +1,14 @@
+//This is not Cpp Problem.
+//This Problem Only Visual Basic
+
+//Module VBModule
+//Sub Main()
+//        Console.WriteLine("      ===================")
+//        Console.WriteLine("          ____||___")
+//        Console.WriteLine("\ /      /       []\")
+//        Console.WriteLine(" X=======           \__")
+//        Console.WriteLine("/ \      \____________|")
+//        Console.WriteLine("            ||  ||")
+//        Console.WriteLine("         \-----------/")
+//    End Sub
+//End Module
