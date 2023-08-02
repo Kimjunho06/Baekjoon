78a1c73c1d14fe5411252204e2b13105f6478b01
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-08-02 21:31:51 +0900

    Boj15651

---
+++ Baekjoon/Boj15651.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+bool arr[10] = {};
+int ar[9] = {};
+void BackTracking(int n, int m, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < n; i++) {
+			if (ar[i] != 0) {
+				cout << ar[i] << " ";
+			}
+		}
+		cout << "\n";
+	}
+	else {
+		for (int i = 1; i <= n; i++) {
+			arr[i] = true;
+			ar[cnt] = i;
+			BackTracking(n, m, cnt + 1);
+			ar[cnt] = 0;
+			arr[i] = false;
+		}
+	}
+}
+
+int main() {
+	int n, m;
+
+	cin >> n >> m;
+
+	BackTracking(n, m, 0);
+}
