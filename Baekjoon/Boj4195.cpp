94f6faf236d9a148a621b7b7a329ecd7c882d579
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-08-11 23:50:20 +0900

    Boj27433

---
+++ Baekjoon/Boj4195.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[1000001];
+int cnt;
+int find(int x) {
+	if (arr[x] == x)
+		return x;
+	return arr[x] = find(arr[x]);
+}
+
+void merge(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y) return;
+	arr[y] = x;
+}
+
+bool IsUnion(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y)
+		return true;
+	return false;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int T, n;
+	string f, s;
+
+	cin >> T;
+	
+	for (int i = 0; i < T; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			cin >> f >> s;
+
+
+
+			cout << cnt << "\n";
+		}
+	}
+
+
+}
