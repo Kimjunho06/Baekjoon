a1a1226f8948cb4f297432ef4c7d8a8c88b33d88
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-08-16 17:46:32 +0900

    Boj4195

--- Baekjoon/Boj4195.cpp
+++ Baekjoon/Boj4195.cpp
@@ -1,49 +1,78 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int arr[1000001];
-//int cnt;
-//int find(int x) {
-//	if (arr[x] == x)
-//		return x;
-//	return arr[x] = find(arr[x]);
-//}
-//
-//void merge(int x, int y) {
-//	x = find(x);
-//	y = find(y);
-//	if (x == y) return;
-//	arr[y] = x;
-//}
-//
-//bool IsUnion(int x, int y) {
-//	x = find(x);
-//	y = find(y);
-//	if (x == y)
-//		return true;
-//	return false;
-//}
-//
-//int main() {
-//	ios_base::sync_with_stdio(false);
-//	cin.tie(nullptr); cout.tie(nullptr);
-//
-//	int T, n;
-//	string f, s;
-//
-//	cin >> T;
-//	
-//	for (int i = 0; i < T; i++) {
-//		cin >> n;
-//		for (int j = 0; j < n; j++) {
-//			cin >> f >> s;
-//
-//
-//
-//			cout << cnt << "\n";
-//		}
-//	}
-//
-//
-//}
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int arr[200001];
+int find(int x) {
+	if (arr[x] == x)
+		return x;
+	return arr[x] = find(arr[x]);
+}
+
+void merge(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y) return;
+	arr[y] = x;
+}
+
+bool IsUnion(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y)
+		return true;
+	return false;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int T, n;
+	int idx;
+	vector<int> v;
+	map<string, int> mp;
+	string str, str2;
+
+	v.resize(200002);
+
+	cin >> T;
+
+	while (T--)
+	{
+		cin >> n;
+
+		mp.clear();
+		idx = 0;
+		for (int i = 0; i < 200001; i++) {
+			arr[i] = i;
+			v[i] = 1;
+		}
+
+		for (int i = 0; i < n; i++) {
+			cin >> str >> str2;
+
+			int a, b;
+			if (mp.find(str) == mp.end()) {
+				mp.insert({ str, idx });
+				idx++;
+			}
+			if (mp.find(str2) == mp.end()) {
+				mp.insert({ str2, idx });
+				idx++;
+			}
+			a = find(mp.find(str)->second);
+			b = find(mp.find(str2)->second);
+
+			if (a != b)
+			{
+				merge(mp.find(str)->second, mp.find(str2)->second);
+				v[a] += v[b];
+			}
+
+			cout << v[a] << "\n";
+		}
+	}
+}
