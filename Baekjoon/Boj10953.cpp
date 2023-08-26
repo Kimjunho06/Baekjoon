2f3375a051d4faa4c6a0f006e5a0737264307d72
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-08-26 20:05:43 +0900

    Boj10953

---
+++ Baekjoon/Boj10953.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	string str;
+
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> str;
+
+		cout << str[0] - '0' + str[2] - '0' << "\n";
+	}
+}
