d92c9a7e37da7b52c3bd5379a311514631ee1177
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-09-23 11:25:52 +0900

    Boj2445

---
+++ Baekjoon/Boj2445.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < (n * 2) - 1; i++) {
+		if (i < n) {
+			for (int j = 0; j <= i; j++) {
+				cout << "*";
+			}
+			for (int j = (n * 2) - ((i + 1) * 2); j > 0; j--) {
+				cout << " ";
+			}
+			for (int j = 0; j <= i; j++) {
+				cout << "*";
+			}
+		}
+		else {
+			for (int j = 0; j < n - (i - n + 1); j++) {
+				cout << "*";
+			}
+			for (int j = 0; j < (i - n + 1) * 2; j++) {
+				cout << " ";
+			}
+			for (int j = 0; j < n - (i - n + 1); j++) {
+				cout << "*";
+			}
+		}
+		cout << "\n";
+	}
+}
